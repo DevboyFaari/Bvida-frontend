cbb9cfdf0f96a95c8e7b964849a9b2aa87d60c3c
Faari <faarijoye@gmail.com>
2024-05-16 00:47:18 +0100

    chore: created landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Landingpage from "@bvida/components/ui/landingpage";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div>You!!</div>
+      <Landingpage />
     </>
   );
 }
---
+++ src/components/ui/landingpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landingpage = () => {
+  return <div>This is the landingpage!!!</div>;
+};
+
+export default Landingpage;
